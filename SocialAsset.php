d615de613585f98709653d9d29c974cadaa539f6
Dang Van Thang <vthang87@gmail.com>
2020-08-07 12:08:04 +0700

    no message

--- SocialAsset.php
+++ SocialAsset.php
@@ -11,30 +11,29 @@ namespace tas\social;
 
 
 use yii\web\AssetBundle;
+use const YII_DEBUG;
 
-class SocialAsset extends AssetBundle
-{
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-        $this->sourcePath                  = __DIR__ . '/assets';
-        $this->publishOptions['forceCopy'] = true;
-    }
-    
-    public $css = [
-        // 'css/chat.css',
-        'css/custom.css',
-    ];
-    
-    public $js = [
-        // 'js/app-bundle.js',
-        'js/functions.js',
-        'js/chat.js',
-    ];
-    public $depends = [
-        'app\assets\AppAsset',
-    ];
+class SocialAsset extends AssetBundle{
+	/**
+	 * @inheritdoc
+	 */
+	public function init(){
+		parent::init();
+		$this->sourcePath                  = __DIR__ . '/assets';
+		$this->publishOptions['forceCopy'] = true;
+	}
+	
+	public $css = [
+		// 'css/chat.css',
+		'css/custom.css',
+	];
+	
+	public $js      = [
+		// 'js/app-bundle.js',
+		YII_DEBUG ? 'js/functions.js' : 'js/functions.min.js',
+		YII_DEBUG ? 'js/chat.js' : 'js/chat.min.js',
+	];
+	public $depends = [
+		'app\assets\AppAsset',
+	];
 }
